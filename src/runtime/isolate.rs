ebc05e917fcd1b6b4154da07f3f263177a4de75c
Kailas Mahavarkar <66670953+KailasMahavarkar@users.noreply.github.com>
2026-03-27 03:56:44 +0530

    chore: fmt fix in isolate.rs

--- src/runtime/isolate.rs
+++ src/runtime/isolate.rs
@@ -118,10 +118,7 @@ impl Isolate {
         base_path.push(pool_uid.to_string());
         fs::create_dir_all(&base_path)?;
 
-        let cgroup = match cgroup::select_cgroup_backend(
-            config.strict_mode,
-            &config.instance_id,
-        ) {
+        let cgroup = match cgroup::select_cgroup_backend(config.strict_mode, &config.instance_id) {
             Ok(cg) => match cg.create(&config.instance_id) {
                 Ok(()) => {
                     if let Some(mem) = config.memory_limit {
